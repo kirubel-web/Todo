436d8c733c16f0c74dd814ddc92ec7146cf6f6f9
kirubel-web <kirubelalemu119@gmail.com>
2023-10-24 13:07:46 +0300

    heroku allowed host and update to requirement.txt

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!+se2i2&soym!02)u^+-&s)tsw4-s6afdgv=4uf(3&yy_-l)2u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost","127.0.0.1"]
 
 
 # Application definition
